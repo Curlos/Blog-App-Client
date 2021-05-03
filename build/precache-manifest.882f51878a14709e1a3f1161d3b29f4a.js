0bc1a60ca36d6ffbb2402811df200331b962a10c
Carlos Martinez <curlosmart@gmail.com>
2021-05-02 23:22:35 -0400

    added build version

---
+++ build/precache-manifest.882f51878a14709e1a3f1161d3b29f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ebbf9e2ca62e9f1bb5addf2ef643bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "046b86cd05ecbb463015",
+    "url": "/static/css/main.ed83d5e3.chunk.css"
+  },
+  {
+    "revision": "571fcdaf09bf42afab80",
+    "url": "/static/js/2.717bb5a4.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.717bb5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046b86cd05ecbb463015",
+    "url": "/static/js/main.a93ff355.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
